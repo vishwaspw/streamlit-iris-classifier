b0aac38af69b5dfd3ed4372a477a7ffbf0b0530b
Vishwas R <152512991+vishwaspw@users.noreply.github.com>
2024-11-19 17:57:23 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+os.environ["STREAMLIT_LOG_LEVEL"] = "debug"
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
