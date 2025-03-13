5ef597018fa0e38b6f3f1467a037e8f49981782d
Vishwas R <152512991+vishwaspw@users.noreply.github.com>
2025-03-13 19:08:45 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -12,7 +12,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.decomposition import PCA
 from datetime import datetime
-st.logo("logo.jpg")
 # Initialize database
 def init_db():
     conn = sqlite3.connect("user_data.db")
